8dc87b2947300c267c07667245edf532d8763b8c
piyush7983 <154682036+piyush7983@users.noreply.github.com>
2026-01-20 11:49:13 +0530

    created

---
+++ src/test/java/com/example/day3studentmanagementsystem/Day3StudentmanagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3StudentmanagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
