d5d1e070f7c5b9e58d4c412045e95de0073389b2
piyush7983 <154682036+piyush7983@users.noreply.github.com>
2026-01-20 11:49:13 +0530

    database added

---
+++ src/main/java/com/example/day3studentmanagementsystem/Model/StudentModel.java
@@ -0,0 +1,23 @@
+package com.example.day3studentmanagementsystem.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.lang.annotation.Documented;
+
+@Data
+@AllArgsConstructor //makes constructor of below variables
+@NoArgsConstructor  //for null values
+@Document(collection =  "student" )
+public class StudentModel {
+
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private String email;
+
+}
