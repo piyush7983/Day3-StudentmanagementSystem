f42ed60ffdeeb1d5751575331081622ce8cd2978
piyush7983 <154682036+piyush7983@users.noreply.github.com>
2026-01-27 15:25:49 +0530

    display,delete,update

--- src/main/java/com/example/day3studentmanagementsystem/Controller/StudentController.java
+++ src/main/java/com/example/day3studentmanagementsystem/Controller/StudentController.java
@@ -3,9 +3,9 @@ package com.example.day3studentmanagementsystem.Controller;
 import com.example.day3studentmanagementsystem.Model.StudentModel;
 import com.example.day3studentmanagementsystem.Service.StudentService;
 //import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -23,5 +23,27 @@ public class StudentController {
         return service.addStudent(student);
 
     }
+
+    // Display Student
+
+    @GetMapping("/students")
+    public List<StudentModel> getStudents(){
+        return service.getStudent();
+    }
+
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(@PathVariable String id,@RequestBody StudentModel student){
+        return service.update(id,student);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable String id){
+        service.delete(id);
+    }
+
+
+
+
+
 
 }
